cd7eb3e94bbdf7b350aab46d16c135abba349073
EC2 Default User <ec2-user@ip-172-31-4-67.ec2.internal>
2021-06-09 01:32:40 +0000

    [finish]2-6 編集機能を作ろう

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,10 +1,7 @@
 class TodolistsController < ApplicationController
   def new
-
    @list =List.new
-
   end
-
   def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -21,11 +18,18 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
 
   private
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
-
 end
